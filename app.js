0129c2ebe042cfe1fd9574df8803571e8ca5d6e1
Brian Gunderson - MacBook <brian@bkgunderson.com>
2022-03-05 08:54:41 -0600

    add local css and add navigation EJS partial

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use("/", routes());
 
